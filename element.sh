59ae7e3d0281d573039f886fa5428ce88dd7a8df
ryanzheng0507 <ryanzheng0507@gmail.com>
2024-11-15 09:25:28 +0000

    feat: output for entering valid atomic_number

--- element.sh
+++ element.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuple-only -c"
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
 if [[ -z $1 ]]
 then
@@ -26,3 +26,18 @@ then
 echo "I could not find that element in the database."
 fi
 fi
+
+PROPERTIES_TABLE=$($PSQL "SELECT * FROM properties ORDER BY atomic_number")
+echo "$PROPERTIES_TABLE" | while read ATOMIC_NUMBER BAR ATOMIC_MASS BAR MELTING_POINT_CELSIUS BAR BOILING_POINT_CELSIUS BAR TYPE_ID
+do
+if [[ $ATOMIC_NUMBER = $1 ]]
+then
+ATOMIC_NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number = $ATOMIC_NUMBER")
+NAME_FORMATTED=$(echo $ATOMIC_NAME | sed -r 's/^ *| *$//g')
+TYPE=$($PSQL "SELECT type FROM types INNER JOIN properties USING(type_id) WHERE atomic_number = $ATOMIC_NUMBER")
+TYPE_FORMATTED=$(echo $TYPE | sed -r 's/^ *| *$//g')
+ATOMIC_SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number = $ATOMIC_NUMBER")
+SYMBOL_FORMATTED=$(echo $ATOMIC_SYMBOL | sed -r 's/^ *| *$//g')
+echo "The element with atomic number $ATOMIC_NUMBER is $NAME_FORMATTED ($SYMBOL_FORMATTED). It's a $TYPE_FORMATTED, with a mass of $ATOMIC_MASS amu. $NAME_FORMATTED has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
+fi
+done
