10bef5e6fbad35c993386b3e2ac1b818366189a0
ryanzheng0507 <ryanzheng0507@gmail.com>
2024-11-15 08:39:23 +0000

    feat: output for non-exist data

--- element.sh
+++ element.sh
@@ -4,4 +4,25 @@ PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuple-only -c"
 if [[ -z $1 ]]
 then
 echo "Please provide an element as an argument."
+elif [[ $1 =~ [0-9]+ ]]
+then
+ATOMIC_NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number = $1")
+if [[ -z $ATOMIC_NAME ]]
+then
+echo "I could not find that element in the database."
+fi
+elif [[ $1 =~ [A-Z] ]] || [[ $1 =~ [A-Z][a-z] ]]
+then
+ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE symbol = $1")
+if [[ -z $ATOMIC_NUMBER ]]
+then
+echo "I could not find that element in the database."
+fi
+elif [[ $1 =~ [A-Z][a-z]+ ]]
+then
+ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE name = $1")
+if [[ -z $ATOMIC_NUMBER ]]
+then
+echo "I could not find that element in the database."
+fi
 fi
